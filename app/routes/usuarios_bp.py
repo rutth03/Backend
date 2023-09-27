ea4531a07fc806eab91d09779fbb71ea0d23aa0a
Ruth Carrasco <ruthcarrasco87@gmail.com>
2023-09-27 16:44:44 -0300

    creación rutas  usuario blueprint

---
+++ app/routes/usuarios_bp.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+from ..controllers.usuarios_controller import UserController
+
+usuario_bp = Blueprint('usuario_bp', __name__)
+
+usuario_bp.route('/login', methods=['GET'])(UserController.log_in)
+#usuario_bp.route('/servers/<int:user_id>', methods=['GET'])(UserController.get_all_servers)
+usuario_bp.route('/signIn', methods=['POST'])(UserController.create)
+usuario_bp.route('/<int:user_id>', methods=['PUT'])(UserController.update)
+usuario_bp.route('/<int:user_id>', methods=['DELETE'])(UserController.delete)
